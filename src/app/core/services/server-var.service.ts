4ea7d3968f0f8031933a51f3e8e8b385d3891d17
Marianoantar <marianoantar@gmail.com>
2024-10-01 12:36:27 -0300

    Agregué  urlServer='https://prueba.storum.com.ar'

--- src/app/core/services/server-var.service.ts
+++ src/app/core/services/server-var.service.ts
@@ -7,7 +7,8 @@ export class ServerVarService {
 
   urlLocalServer = 'http://localhost:3000'; // URL del server
   // urlServer = this.urlLocalServer;
-  urlServer = 'https://backend-rapid-food.onrender.com'; // URL del server (ONRENDER)
+  // urlServer = 'https://backend-rapid-food.onrender.com'; // URL del server (ONRENDER)
+  urlServer = 'https://prueba.storum.com.ar'; // URL del server (ONRENDER)
 
   constructor() { }
 }
